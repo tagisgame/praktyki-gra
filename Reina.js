4c33fd67aef82d05d799a3f803ca407c7f3969b8
Renata <r.wesolowska@devesoft.pl>
2019-05-20 10:07:27 +0200

    s

--- Reina.js
+++ Reina.js
@@ -29,3 +29,15 @@ function moveDown(){
 }
 
 getKeyAndMove(e);
+
+function Weapon(id, type, damage){
+  this.id = id;
+  this.type = type;
+  this.damage = (this.type === "axe" ? +20 : (this.type === "deager" ? +1 : dmg));
+}
+
+function Potion(id, type, effect){
+  this.id = id;
+  this.type = type;
+  this.effect = (this.type === "poison" ? -5 : (this.type === "heal" ? +5 : HP));
+}
