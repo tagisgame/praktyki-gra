293fc404a3e2e34facc2da7336d0c4bd6408c91e
Renata <r.wesolowska@devesoft.pl>
2019-05-20 09:20:58 +0200

    funkcja ruchu gracza

---
+++ Reina.js
@@ -0,0 +1,31 @@
+function getKeyAndMove(e){
+				var key_code=e.which||e.keyCode;
+				switch(key_code){
+					case 37: //left arrow key
+						moveLeft();
+						break;
+					case 38: //Up arrow key
+						moveUp();
+						break;
+					case 39: //right arrow key
+						moveRight();
+						break;
+					case 40: //down arrow key
+						moveDown();
+						break;
+				}
+}
+function moveLeft(){
+		;
+}
+function moveUp(){
+		;
+}
+function moveRight(){
+		;
+}
+function moveDown(){
+		;
+}
+
+getKeyAndMove(e);
